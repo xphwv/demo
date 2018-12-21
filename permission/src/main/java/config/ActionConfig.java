9f96529aaa8e81805b347eb656712cf86bd1b15a
xphwv <xphwv@126.com>
2018-12-21 10:47:33 +0800

    fastJson update

--- permission/src/main/java/config/ActionConfig.java
+++ permission/src/main/java/config/ActionConfig.java
@@ -4,18 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public abstract class ActionConfig extends ActionResult {
+public abstract class ActionConfig  {
 	protected HttpServletRequest request;
 	protected HttpServletResponse response;
 
 	public ActionConfig() {
-		this.request = beat().getRequest();
-		this.response = beat().getResponse();
-	}
+    }
 
 	
 
-	@Override
 	public void render() {
 		try {
 			action();
